5e5e90ab84387e9076730fa1afb80b3b354b9f83
DxDzere <74333816+DxDzere@users.noreply.github.com>
2023-04-24 22:03:54 -0300

    ejercicio 3 finalizado

--- Ejercicio_3/src/ejercicio_3/Ejercicio_3.java
+++ Ejercicio_3/src/ejercicio_3/Ejercicio_3.java
@@ -4,18 +4,100 @@
  * and open the template in the editor.
  */
 package ejercicio_3;
-
+import ejercicio_3.Entidad.Persona;
+import ejercicio_3.Servicios.PersonaServicio;
 /**
  *
  * @author Zere
  */
 public class Ejercicio_3 {
+    static void porcentajeMayores(boolean[] bool) {
+
+        double cont = 0;
+
+        for (int i = 0; i < bool.length; i++) {
+
+            if (bool[i] == true) {
+                cont++;
+            }
+
+        }
+        System.out.println("El promedio de mayores de edad es: " + (cont / bool.length) * 100 + " % ");
+
+    }
+
+    static void promediopeso(int[] n) {
+        double cont1 = 0, cont2 = 0, cont3 = 0;
+        for (int i = 0; i < n.length; i++) {
+            switch (n[i]) {
+                case -1:
+                    cont1++;
+                    break;
+                case 0:
+                    cont2++;
+                    break;
+                case 1:
+                    cont3++;
+                    break;
+
+            }
+
+        }
+        System.out.println("el promedio de personas con sobre peso es = " + (cont3 / n.length) * 100 + " % ");
+        System.out.println("el promedio de personas con peso ideal = " + (cont2 / n.length) * 100 + " % ");
+        System.out.println("el promedio de personas con bajo peso es = " + (cont1 / n.length) * 100 + " % ");
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        PersonaServicio ps = new PersonaServicio();
+
+        Persona p1 = ps.crearPersona();
+        Persona p2 = ps.crearPersona();
+        Persona p3 = ps.crearPersona();
+        Persona p4 = ps.crearPersona();
+
+        boolean[] mayoresOmenores = new boolean[4];
+        int[] pesoIdeal = new int[4];
+
+        for (int i = 0; i < mayoresOmenores.length; i++) {
+            switch (i) {
+                case 0:
+                    mayoresOmenores[i] = ps.esMayorDeEdad(p1);
+                    break;
+                case 1:
+                    mayoresOmenores[i] = ps.esMayorDeEdad(p2);
+                    break;
+                case 2:
+                    mayoresOmenores[i] = ps.esMayorDeEdad(p3);
+                    break;
+                case 3:
+                    mayoresOmenores[i] = ps.esMayorDeEdad(p4);
+                    break;
+            }
+        }
+
+        for (int i = 0; i < pesoIdeal.length; i++) {
+            switch (i) {
+                case 0:
+                    pesoIdeal[i] = ps.calculrIMC(p1);
+                    break;
+                case 1:
+                    pesoIdeal[i] = ps.calculrIMC(p2);
+                    break;
+                case 2:
+                    pesoIdeal[i] = ps.calculrIMC(p3);
+                    break;
+                case 3:
+                    pesoIdeal[i] = ps.calculrIMC(p4);
+                    break;
+            }
+
+        }
+        porcentajeMayores(mayoresOmenores);
+        promediopeso(pesoIdeal);
     }
-    
 }
--- Ejercicio_3/src/ejercicio_3/Servicios/PersonaServicio.java
+++ Ejercicio_3/src/ejercicio_3/Servicios/PersonaServicio.java
@@ -5,10 +5,66 @@
  */
 package ejercicio_3.Servicios;
 
+import ejercicio_3.Entidad.Persona;
+import java.util.Scanner;
+
 /**
  *
  * @author Zere
  */
 public class PersonaServicio {
+    private Scanner leer = new Scanner(System.in).useDelimiter("\n");
     
+    
+    //Método esMayorDeEdad(): indica si la persona es mayor de edad. La función devuelve 
+    //un booleano.
+
+    public boolean esMayorDeEdad(Persona p){
+        return p.getEdad()>18;
+    }
+    
+    //Metodo crearPersona(): el método crear persona, le pide los valores de los atributos al usuario
+    //y después se le asignan a sus respectivos atributos para llenar el objeto Persona. Además, 
+    //comprueba que el sexo introducido sea correcto, es decir, H, M o O. Si no es correcto se 
+    //deberá mostrar un mensaje
+    public Persona crearPersona(){
+        System.out.println("Ingrese el nombre de la persona");
+        String nombre= leer.next();
+        System.out.println("Ingrese el DNI de la persona");
+        long dni = leer.nextLong();
+        System.out.println("Ingrese la edad");
+        int edad= leer.nextInt();
+        System.out.println("Ingrese peso");
+        double peso= leer.nextDouble();
+        System.out.println("Ingrese la altura");
+        double altura= leer.nextDouble();
+        System.out.println("ingrese el sexo (H|M|O)");
+        char sexo= leer.next().toUpperCase().charAt(0);
+        
+        while(sexo!='H' && sexo!='M' && sexo !='O'){
+            System.out.println("Caracter incorrecto, Ingrese nuevamente el sexo (H|M|O) ");
+             sexo= leer.next().toUpperCase().charAt(0);     
+      
+        }
+        return new Persona( nombre,edad,altura,sexo,peso,dni);
+    }
+    //Método /*calcularIMC(): calculara si la persona está en su peso ideal 
+    //(peso en kg/(altura^2 en mt2)). Si esta fórmula da por resultado un valor
+    //      menor que 20, significa que la persona está por debajo de su peso ideal 
+    //    y la función devuelve un -1. Si la fórmula da por resultado un número entre
+    //  20 y 25 (incluidos), significa que la persona está en su peso ideal y la función 
+    //devuelve un 0. Finalmente, si el resultado de la fórmula es un valor mayor que 25 
+    //significa que la persona tiene sobrepeso, y la función devuelve un 1.
+
+    public int calculrIMC(Persona p){
+        
+        if((p.getPeso()/Math.pow(p.getAltura(),2)<20)){
+           return-1;
+           
+        }else if((p.getPeso()/Math.pow(p.getAltura(),2))<=25){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
 }
